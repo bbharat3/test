d7583002e4d428fad748c8f6ff725b4020f94fe6
knightmare <bbharat3@gmail.com>
2016-05-07 23:54:28 +0530

    lwrp test

---
+++ lwrp/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: lwrp
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+lwrp_add "adding" do
+  #name 'add'
+  number1 2
+  number2 4
+  action :add
+end
